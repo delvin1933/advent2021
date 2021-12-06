3c1a9cd7053a11236abdbfb9df6a80aa9e1b64e2
Delvin <cfarges@gmail.com>
2021-12-06 21:39:25 +0100

    update day6, no need to read

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -14,19 +14,31 @@ where
 }
 
 fn main() {
-    let lines = read_lines("./input.txt").unwrap_or_default();
+    //let lines = read_lines("./input.txt").unwrap_or_default();
 
-    let lanterns: Vec<usize> = parse_lanterns(&lines[0]);
+    //let lanterns: Vec<usize> = parse_lanterns(&lines[0]);
+    let lanterns: Vec<usize> = vec![
+        2, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 5, 1, 1, 3, 5, 1, 1, 3, 1, 1, 3, 1, 4, 4, 4, 5, 1, 1,
+        1, 3, 1, 3, 1, 1, 2, 2, 1, 1, 1, 5, 1, 1, 1, 5, 2, 5, 1, 1, 2, 1, 3, 3, 5, 1, 1, 4, 1, 1,
+        3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 5, 1, 2, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 5, 1,
+        1, 1, 4, 5, 1, 1, 3, 4, 1, 1, 1, 3, 5, 1, 1, 1, 2, 1, 1, 4, 1, 4, 1, 2, 1, 1, 2, 1, 5, 1,
+        1, 1, 5, 1, 2, 2, 1, 1, 1, 5, 1, 2, 3, 1, 1, 1, 5, 3, 2, 1, 1, 3, 1, 1, 3, 1, 3, 1, 1, 1,
+        5, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3, 1, 1, 4, 1, 1, 3, 2, 1, 2, 1, 1, 2, 2, 1, 2, 1,
+        1, 1, 4, 1, 2, 4, 1, 1, 4, 4, 1, 1, 1, 1, 1, 4, 1, 1, 1, 2, 1, 1, 2, 1, 5, 1, 1, 1, 1, 1,
+        5, 1, 3, 1, 1, 2, 3, 4, 4, 1, 1, 1, 3, 2, 4, 4, 1, 1, 3, 5, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1,
+        5, 3, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1,
+        1, 1, 1, 5, 1, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 3, 1, 4, 1, 1, 2, 2, 2, 1, 1, 2, 1, 1,
+    ];
 
-    println!(" {:?}", lanterns.len());
+    //println!(" {:?}", lanterns.len());
 
     let mut lanterns = count_lanterns(lanterns);
 
-    println!(" {:?}", lanterns);
+    //println!(" {:?}", lanterns);
 
     let lanterns = grow(256, lanterns);
 
-    println!(" {:?}", lanterns);
+    //println!(" {:?}", lanterns);
     let nb_lanterns: i128 = lanterns.iter().sum();
 
     println!("Part2 {}", nb_lanterns);
