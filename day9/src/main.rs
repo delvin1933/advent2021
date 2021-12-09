7b4eace2bcc8b81438303fa44ab9d356c4fb1d91
Delvin <cfarges@gmail.com>
2021-12-09 15:31:30 +0100

    add day9 part2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -17,9 +18,25 @@ fn main() {
     let lines = read_lines("./input.txt").unwrap_or_default();
     let map = parse_input(lines);
 
-    let lowers = find_lowers(map);
+    let lowers = find_lowers(&map);
 
-    println!("Part 1 : {}", calc_risk(lowers));
+    println!("Part 1 : {}", calc_risk(&lowers, &map));
+
+    let mut part2_points: Vec<HashSet<Point>> = Vec::new();
+    for point in lowers {
+        part2_points.push(part2_neighbours(point, &map));
+        //lowers.iter().map(|l| ).collect();
+    }
+
+    let mut part2_counts: Vec<usize> = part2_points.iter().map(|s| s.len()).collect();
+
+    part2_counts.sort();
+
+    let count = part2_counts[part2_counts.len() - 1]
+        * part2_counts[part2_counts.len() - 2]
+        * part2_counts[part2_counts.len() - 3];
+
+    println!("Part 2 : {}", count);
 }
 
 fn parse_input(input: Vec<String>) -> Vec<Vec<i32>> {
@@ -35,22 +52,19 @@ fn parse_input(input: Vec<String>) -> Vec<Vec<i32>> {
 
     result
 }
+
+#[derive(PartialEq, Eq, Hash, Copy, Clone, Debug)]
 struct Point {
     x: usize,
     y: usize,
-    value: i32,
 }
 
-fn find_lowers(map: Vec<Vec<i32>>) -> Vec<Point> {
+fn find_lowers(map: &Vec<Vec<i32>>) -> Vec<Point> {
     let mut result: Vec<Point> = Vec::new();
 
     for (y, yline) in map.iter().enumerate() {
-        for (x, value) in yline.iter().enumerate() {
-            let current = Point {
-                x,
-                y,
-                value: map[y][x],
-            };
+        for (x, _value) in yline.iter().enumerate() {
+            let current = Point { x, y };
             if is_low_point(&current, &map) {
                 result.push(current);
             }
@@ -115,26 +129,67 @@ fn is_low_point(point: &Point, map: &Vec<Vec<i32>>) -> bool {
         && map[point.y][point.x] < lower_num;
 }
 
-fn calc_risk(points: Vec<Point>) -> i32 {
-    points.iter().map(|p| p.value + 1).sum()
+fn calc_risk(points: &Vec<Point>, map: &Vec<Vec<i32>>) -> i32 {
+    points.iter().map(|p| map[p.y][p.x] + 1).sum()
 }
 
-fn reduce_map(map: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    let mut result: Vec<Vec<i32>> = Vec::new();
+fn part2_neighbours(point: Point, map: &Vec<Vec<i32>>) -> HashSet<Point> {
+    let mut point_set: HashSet<Point> = HashSet::new();
+    get_neighbours_inner(&mut point_set, &point, map);
+    point_set
+}
 
-    for yline in map {
-        let mut line: Vec<i32> = Vec::new();
-        for value in yline {
-            if value == 9 {
-                line.push(1);
-            } else {
-                line.push(0);
+fn get_neighbours_inner(input_set: &mut HashSet<Point>, point: &Point, map: &Vec<Vec<i32>>) {
+    if !input_set.contains(point) {
+        if map[point.y][point.x] != 9 {
+            if !input_set.contains(point) {
+                input_set.insert(*point);
+            }
+
+            if point.y > 0 {
+                get_neighbours_inner(
+                    input_set,
+                    &Point {
+                        x: point.x,
+                        y: point.y - 1,
+                    },
+                    map,
+                );
+            }
+            if point.y < map.len() - 1 {
+                get_neighbours_inner(
+                    input_set,
+                    &Point {
+                        x: point.x,
+                        y: point.y + 1,
+                    },
+                    map,
+                );
+            }
+
+            if point.x > 0 {
+                get_neighbours_inner(
+                    input_set,
+                    &Point {
+                        x: point.x - 1,
+                        y: point.y,
+                    },
+                    map,
+                );
+            }
+
+            if point.x < map[0].len() - 1 {
+                get_neighbours_inner(
+                    input_set,
+                    &Point {
+                        x: point.x + 1,
+                        y: point.y,
+                    },
+                    map,
+                );
             }
         }
-        result.push(line);
     }
-
-    result
 }
 
 #[cfg(test)]
@@ -153,7 +208,23 @@ mod tests {
 
         let map = parse_input(lines);
         println!("{:?}", map);
-        let lowers = find_lowers(map);
-        assert_eq!(15, calc_risk(lowers))
+        let lowers = find_lowers(&map);
+        assert_eq!(15, calc_risk(&lowers, &map));
+
+        let mut part2_points: Vec<HashSet<Point>> = Vec::new();
+        for point in lowers {
+            part2_points.push(part2_neighbours(point, &map));
+            //lowers.iter().map(|l| ).collect();
+        }
+
+        let mut part2_counts: Vec<usize> = part2_points.iter().map(|s| s.len()).collect();
+
+        part2_counts.sort();
+        let mut count = 1;
+
+        count = part2_counts[part2_counts.len() - 1]
+            * part2_counts[part2_counts.len() - 2]
+            * part2_counts[part2_counts.len() - 3];
+        assert_eq!(1134, count)
     }
 }
